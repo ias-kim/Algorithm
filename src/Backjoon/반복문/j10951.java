0c2864c9fce349598ccf65d534169d3b1d03cc65
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2024-12-29 10:31:28 +0900

    buffredReader

---
+++ src/Backjoon/반복문/j10951.java
@@ -0,0 +1,30 @@
+package Backjoon.반복문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class j10951 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+
+        while ( (str=br.readLine()) != null ) {
+
+            st = new StringTokenizer(str," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+
+        }
+        System.out.print(sb);
+
+
+    }
+}
+
