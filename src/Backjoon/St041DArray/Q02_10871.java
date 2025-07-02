1137abbb0f18b20fd8e9cb1270960538773f0ae0
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-07-02 14:48:12 +0900

    feat: 백준 1차원 배열 문제 풀이

---
+++ src/Backjoon/St041DArray/Q02_10871.java
@@ -0,0 +1,29 @@
+package Backjoon.St041DArray;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+
+public class Q02_10871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int find = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] < find) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
