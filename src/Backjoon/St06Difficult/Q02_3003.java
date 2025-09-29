7a55117482060d658af4ff51fad59bbbae425df4
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-09-29 13:00:40 +0900

    백준 심화문제 풀이

---
+++ src/Backjoon/St06Difficult/Q02_3003.java
@@ -0,0 +1,17 @@
+package Backjoon.St06Difficult;
+
+import java.util.Scanner;
+
+public class Q02_3003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int chess[] = {1, 1, 2, 2, 2, 8};
+        int input[] = new int[6];
+        for (int i = 0; i < chess.length; i++) {
+            input[i] = sc.nextInt();
+        }
+        for (int i = 0; i < input.length; i++) {
+            System.out.println(chess[i] - input[i] + " ");
+        }
+    }
+}
