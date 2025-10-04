167049293b253bcc5588cfc763ca8f45c8c477a9
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-10-04 11:09:18 +0900

    백준 심화1 문제 2444 별 찍기 - 7

---
+++ src/Backjoon/St06Difficult/Q03_2444.java
@@ -0,0 +1,30 @@
+package Backjoon.St06Difficult;
+
+import java.util.Scanner;
+
+public class Q03_2444 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= n - 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= ((n - i) * 2 - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
